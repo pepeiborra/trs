64634eb6de1e05229c8af30f090208890f3eb413
pepe <pepe@ender4.dsic.upv.es>
2009-01-26 19:58:25 +0100

    Add an Observe instance for rules
    While I was there, I refactored RuleG to RuleF
    
    darcs-hash:20090126185825-26539-bd0b60be2c8baf1cea5867c660bf8c69195e4c38.gz

--- src/TRS/Rules.hs
+++ src/TRS/Rules.hs
@@ -13,41 +13,50 @@ import Data.Maybe
 import Data.Traversable
 
 import TRS.Types
+
+#ifdef HOOD
+import Debug.Observe
+#endif //HOOD
+
 ----------
 -- * Rules
 ----------
-data RuleG a = !a :-> !a deriving Eq
+data RuleF a = !a :-> !a deriving Eq
 
-lhs,rhs :: forall t. RuleG t -> t
+lhs,rhs :: forall t. RuleF t -> t
 lhs (l :-> _) = l
 rhs (_ :-> r) = r
 
-type Rule f = RuleG (Term f)
+type Rule f = RuleF (Term f)
 
 infix 1 :->
 
 -- Sort first on lhs, then on rhs
-instance (Eq (RuleG a),Ord a) => Ord (RuleG a) where
+instance (Eq (RuleF a),Ord a) => Ord (RuleF a) where
   compare (l1 :-> r1) (l2 :-> r2) = case compare l1 l2 of
                                       EQ -> compare r1 r2
                                       x  -> x
 
-instance Traversable RuleG where
+instance Traversable RuleF where
   traverse f (l :-> r) = (:->) <$> f l <*> f r
 
-instance Foldable RuleG where
+instance Foldable RuleF where
   foldMap = foldMapDefault
 
-instance Functor RuleG where
+instance Functor RuleF where
   fmap = fmapDefault
 
 instance SizeF f => Size (Rule f) where size = Data.Foldable.sum . fmap size
 
 --swap :: Rule r s -> Rule r s
-swapRule :: RuleG t -> RuleG t
+swapRule :: RuleF t -> RuleF t
 swapRule (lhs:->rhs) = rhs:->lhs
 
-instance Show (a) => Show (RuleG (a)) where
+instance Show (a) => Show (RuleF (a)) where
     show (a:->b) = show a ++ " -> " ++ show b
 
 instance NFData (f(Term f)) => NFData (Rule f) where rnf (l :-> r) = rnf l `seq` rnf r `seq` ()
+
+#ifdef HOOD
+instance Show a => Observable (RuleF a) where observer = observeBase
+#endif //HOOD
